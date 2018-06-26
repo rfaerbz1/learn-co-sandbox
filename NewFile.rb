97136d8b20282fdd19798b98e6fd2dc76a10cd40
rfaerbz1 <bfaerber@cox.net>
2018-06-26 23:55:23 +0000

    automatically backed up by learn

--- NewFile.rb
+++ NewFile.rb
@@ -26,7 +26,7 @@ class CodeMessage
   end
   
   def decodeMessage
-    print("Input the message you want to be coded\n\t")
+    print("Input the message you want to decode\n\t")
     message = gets.chomp.downcase;
     message = message.to_s # makes the message a string
     
@@ -36,21 +36,22 @@ class CodeMessage
   
     wordArray.each do|word|
       letterArray[0] = (word.split("")) # makes an array with each letter being a seperate value
-      letterArray[0].push(letterArray[0][0], " ") # puts the first letter and a space at the end
-      letterArray[0].delete_at(0) # deletes the first letter at the beginning
+      letterArray[0].insert(0, letterArray[0][letterArray[0].length - 1]) # puts the last letter and puts it at the beginning
+      letterArray[0].delete_at(letterArray[0].length - 1) # deletes the last letter
+      letterArray[0].push(" ") # adds a space to the end of the word
     
       letterArray[0].each do|letter|
         codedMessage += letter
       end
     end
-    print("\nCoding your message...")
+    print("\nDecoding your message...")
     sleep(2)
     print("\nPrinting your message...")
     sleep(1)
-    print("\nYour coded message is:")
+    print("\nYour decoded message is:")
     print("\n\t" + codedMessage + "\n")
   end
 end
 
 code = CodeMessage.new
-code.codeMessage
+code.decodeMessage
